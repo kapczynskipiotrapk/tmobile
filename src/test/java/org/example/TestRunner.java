d118cdfee831c7c89b276b1a1f08de7b3a4579e4
Piotrek <kapczynskipiotr.wwa@gmail.com>
2026-05-08 08:51:37 +0200

    added hooks and reporting

--- src/test/java/org/example/TestRunner.java
+++ src/test/java/org/example/TestRunner.java
@@ -5,6 +5,15 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/features", glue = "org.example.steps", monochrome = true)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "org.example.steps",
+        plugin = {
+                "pretty",
+                "html:target/cucumber-report.html",
+                "json:target/cucumber.json"
+        },
+        monochrome = true
+)
 public class TestRunner {
 }
